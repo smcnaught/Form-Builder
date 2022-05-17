bc2df994964bbdf2b2f81424525e9064c1eaed67
Stephanie McNaught <stephaniemcnaught@gmail.com>
2022-05-17 12:35:23 -0600

    getting the right move to and move from col/rows to log when drag/dropping

--- Angular-Form-Builder/src/app/app.component.ts
+++ Angular-Form-Builder/src/app/app.component.ts
@@ -1,65 +1,91 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
-
-interface IDragItem {
-  id: number;
-  columnZero: string;
-  columnOne: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  private formData: IDragItem[] = [
-    { id: 0, columnZero: 'col0-0', columnOne: 'col1-0' },
-    { id: 1, columnZero: 'col0-1', columnOne: 'col1-1' },
-    { id: 2, columnZero: 'col0-2', columnOne: 'col1-2' },
-    { id: 3, columnZero: 'col0-3', columnOne: 'col1-3' },
-  ]
+export class AppComponent implements OnInit {
+  @ViewChild(MatTable) table: MatTable<any>; // TODO this type should be the same type as formData (without the array).
+  
+  public displayedColumns: string[] = ['column0', 'column1'];
+  public formData: any[];
 
-  public displayedColumns: string[] = ['columnZero', 'columnOne'];
-  public displayedForm: IDragItem[] = this.formData;
-  public dragDisabled = true;
-  @ViewChild(MatTable) table: MatTable<IDragItem>;
+  private dragInfo = { draggedItemColumn: 0, draggedItemRow: 0, moveToColumn: 0, moveToRow: 0 };
 
-  public onDrop(event: any, index1: number, index2: number, isRow: boolean): void {
-    if (isRow) {
-      const moveToIndex = Math.max(...[index1, index2]);
-      event.preventDefault();
-      const draggedItemId: number = +event.dataTransfer.getData('text');
+  public ngOnInit(): void {
+    this.formData = [
+      { column0: 'text box', column1: 'multiple choice' }, // row 0
+      { column0: 'checkboxes', column1: 'user input' }, // row 1
+      { column0: 'picture of cat', column1: 'description' }, // row 2
+    ]
+
+  }
+
+  public onDrop(event: any): void {
+    const movingWithinColumn: boolean = this.dragInfo.draggedItemColumn === this.dragInfo.moveToColumn;
+    const draggingDown: boolean = this.dragInfo.draggedItemRow < this.dragInfo.moveToRow;
+
+    if (movingWithinColumn) {
+      if (draggingDown) {
+        this.dragInfo.moveToRow--;
+      }
+    }
+
+    console.log(this.dragInfo.draggedItemColumn); // column it's coming from: 
+    console.log(this.dragInfo.draggedItemRow); // what row it's coming from: 
+    console.log(this.dragInfo.moveToColumn); // what column it's going to:
+    console.log(this.dragInfo.moveToRow); // what row it's going to:
+
+
+
+    if (this.dragInfo.moveToRow > this.displayedColumns.length) {
+      // add new row
+    }
+
+
+
+    // LEAVING this here for reference, but can probably delete soon. It doesn't work for everything.
+    // if (droppingOnRow) {
+    //   const moveToIndex = Math.max(...[index1, index2]);
+    //   event.preventDefault();
       
-      // Add new item
-      const itemToMove = this.formData.find((item: IDragItem) => item.id === +draggedItemId);
-      this.formData.splice(moveToIndex, 0, itemToMove as IDragItem);
+    //   // Add new item
+    //   const itemToMove = this.formData.find((item: IDragItem) => item.id === this.dragInfo.draggedItemId);
+    //   this.formData.splice(moveToIndex, 0, itemToMove as IDragItem);
   
-      // remove old item
-      this.formData = this.formData.filter((item: IDragItem, index: number) => item.id !== draggedItemId || index === moveToIndex);
+    //   // remove old item
+    //   this.formData = this.formData.filter((item: IDragItem, index: number) => item.id !== this.dragInfo.draggedItemId || index === moveToIndex);
   
-      this.displayedForm = this.formData;
-      this.table.renderRows();
-    }
+    //   this.table.renderRows();
+    // }
   }
 
-  public onDragStart(event: DragEvent, item: IDragItem): void {
+  public onDragStart(event: DragEvent, columnIndex: number, rowIndex: number): void {
     if (event) {
-      (<any>event)
-        .dataTransfer
-        .setData('text/plain', item.id);
-  
-      // (<any>event).currentTarget.style.backgroundColor = 'yellow';
+      this.dragInfo.draggedItemColumn = columnIndex;
+      this.dragInfo.draggedItemRow = rowIndex;
     }
   }
 
-  public onDragOver(event: any, draggedOverId: string) {
+  public onDragOver(event: any, rowIndex: number, columnIndex: number) {
     event.preventDefault();
-    // event.target.style.border = "10px solid blue"
+    this.dragInfo.moveToRow = rowIndex;
+    this.dragInfo.moveToColumn = columnIndex;
   }
 
   public onDragLeave(event: any): void {
     event.preventDefault();
-    // event.target.style.border = "none";
+  }
+
+  private addColumn(columnID: number): void {
+    const columnName: string = 'column' + columnID;
+    this.formData.forEach((row) => {
+      row[columnName] = null;
+    })
+
+    this.displayedColumns.push(columnName);
+    this.table.renderRows();
   }
 }
